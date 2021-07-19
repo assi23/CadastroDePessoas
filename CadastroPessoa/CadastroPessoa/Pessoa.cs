06cb2e2b0a80c34859cbb00b957e2e9b7cb0a79b
assi23 <guiassi@hotmail.com>
2021-07-19 10:09:52 -0300

    criação de classes

---
+++ CadastroPessoa/CadastroPessoa/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa
+{
+	class Pessoa
+	{
+		public int ID{ get; set; }
+		public string Nome { get; set; }
+		public DateTime DataNasc { get; set; }
+		public string Email { get; set; }
+		public Endereco Endereco { get; set; }
+	}
+}
---
+++ CadastroPessoa/CadastroPessoa/PessoaFisica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa
+{
+	class PessoaFisica : Pessoa
+	{
+		public string RG{ get; set; }
+		public string CPF{ get; set; }
+	}
+}
